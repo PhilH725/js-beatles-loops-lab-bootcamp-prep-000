8c460a18bbd79c2f515e2f98a4a78b9a76e5df0d
Phil Hartley <philh725@gmail.com>
2018-10-06 03:17:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var array = {}
 
 function theBeatlesPlay(musicians, instruments) {
   for (var i = 0; i < musicians.length; i ++) {
-    array[i] = musicians[i][instruments[i]]
+    array.push("${musicians[i]} ${instruments[i]")
   }
   return array
 }
