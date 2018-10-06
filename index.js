5bb7f8d8eb2be3ca5f09eb83d4d56ca451126814
Phil Hartley <philh725@gmail.com>
2018-10-06 03:24:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var array = []
 function theBeatlesPlay(musicians, instruments) {
   for (var i = 0; i < musicians.length; i ++) {
     array.push(musicians[i] + ' plays ' + instruments[i])
+    /* `` characters weren't working. i guess thats a problem
+    with needing to update my browser? this works too i guess. */
   }
   return array
 }
+
